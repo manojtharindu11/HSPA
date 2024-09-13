85f0fe5f64d074078b6f4b68e9684b3709ab274d
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-09-13 20:32:00 +0530

    create post and delete method to add and delete cities

--- backend/Controllers/CityController.cs
+++ backend/Controllers/CityController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using web_api.Data;
+using web_api.Models;
 
 namespace web_api.Controllers
 {
@@ -16,9 +18,9 @@ namespace web_api.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetCities()
+        public async Task<IActionResult> GetCities()
         {
-            var cities = _dataContext.Cities.ToList();
+            var cities = await _dataContext.Cities.ToListAsync();
             if (cities.Any())
             {
                 return Ok(cities);
@@ -30,6 +32,39 @@ namespace web_api.Controllers
         public string Get(int id)
         {
             return "Atlanta";
+        }
+
+        [HttpPost("add")]
+        [HttpPost("add/{cityName}")]
+
+        public async Task<IActionResult> AddCity(string cityName)
+        {
+            City city = new City();
+            city.Name = cityName;
+            await _dataContext.Cities.AddAsync(city);
+            await _dataContext.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        [HttpPost("post")]
+
+
+        public async Task<IActionResult> AddCity(City city)
+        {
+            await _dataContext.Cities.AddAsync(city);
+            await _dataContext.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        [HttpDelete("delete{id}")]
+
+
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await _dataContext.Cities.FindAsync(id);
+            _dataContext.Cities.Remove(city);
+            await _dataContext.SaveChangesAsync();
+            return Ok(city);
         }
     }
 }
