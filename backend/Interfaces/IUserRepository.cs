c12017b4f0a5ce7d90886e1b2d060fc10093232c
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-09-27 17:00:56 +0530

    create register method

--- backend/Interfaces/IUserRepository.cs
+++ backend/Interfaces/IUserRepository.cs
@@ -5,5 +5,10 @@ namespace web_api.Interfaces
     public interface IUserRepository
     {
         Task<User> Authenticate(string username, string password);
+
+        void Register(string username, string password);
+        Task<bool> UserAlreadyExist(string username);
+
+
     }
 }
--- backend/Repo/UserRepository.cs
+++ backend/Repo/UserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 using web_api.Data;
 using web_api.Interfaces;
 using web_api.Models;
@@ -16,7 +17,31 @@ namespace web_api.Repo
 
         public async Task<User> Authenticate(string username, string password)
         {
-            return await dc.Users.FirstOrDefaultAsync(x => x.UserName == username && x.Password == password);
+            //return await dc.Users.FirstOrDefaultAsync(x => x.UserName == username && x.Password == password);
+            return await dc.Users.FirstOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public void Register(string username, string password)
+        {
+            byte[] passwordHash, passwordKey;
+
+            using( var hmac = new HMACSHA512())
+            {
+                passwordKey = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+            User user = new User();
+            user.UserName = username;
+            user.Password = passwordHash;
+            user.PasswordKey = passwordKey;
+
+            dc.Users.Add(user);
+        }
+
+        public async Task<bool> UserAlreadyExist(string username)
+        {
+            return await dc.Users.AnyAsync(x => x.UserName == username);
         }
     }
 }
