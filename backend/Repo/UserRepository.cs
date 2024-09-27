eccab659fd2d2818ab4d9babd1ff8c38e1cdf90c
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-09-27 20:38:53 +0530

    create password hasher

--- backend/Repo/UserRepository.cs
+++ backend/Repo/UserRepository.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
+using System.Text;
 using web_api.Data;
 using web_api.Interfaces;
 using web_api.Models;
@@ -15,10 +17,35 @@ namespace web_api.Repo
             dc = dataContext;
         }
 
-        public async Task<User> Authenticate(string username, string password)
+        public async Task<User> Authenticate(string username, string passwordText)
         {
             //return await dc.Users.FirstOrDefaultAsync(x => x.UserName == username && x.Password == password);
-            return await dc.Users.FirstOrDefaultAsync(x => x.UserName == username);
+            var user = await dc.Users.FirstOrDefaultAsync(x => x.UserName == username);
+
+            if (user == null || user.Password == null)
+            {
+                return null;
+            }
+
+            if (!MatchPasswordHash(passwordText, user.Password, user.PasswordKey)) { return null; }
+
+            return user;
+        }
+
+        private bool MatchPasswordHash(string passwordText, byte[] password, byte[] passwordKey)
+        {
+            using (var hmac = new HMACSHA512(passwordKey))
+            {
+                var passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passwordText));
+
+                for (int i = 0;i < passwordHash.Length;i++)
+                {
+                    if (passwordHash[i] != password[i])
+                        return false;
+                }
+
+                return true;
+            }
         }
 
         public void Register(string username, string password)
