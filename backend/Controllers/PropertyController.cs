150fe52226c0b74f82524c05d615dc20810fa92f
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-09-30 14:42:35 +0530

    create property controller

---
+++ backend/Controllers/PropertyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web_api.Interfaces;
+
+namespace web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyController : BaseController
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public PropertyController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        [HttpGet("type/{sellRent}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetPropertyList(int sellRent)
+        {
+            var properties = await unitOfWork.propertyRepository.GetAllPropertiesAsync(sellRent);
+            return Ok(properties);
+        }
+    }
+}
