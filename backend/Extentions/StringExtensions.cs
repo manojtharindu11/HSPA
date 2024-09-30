9485967c3f09f1ef5be349380af76db856ce2cc6
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-09-30 11:22:12 +0530

    generate method for check the null of string

---
+++ backend/Extentions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace web_api.Extentions
+{
+    public static class StringExtensions
+    {
+        public static bool IsEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s.Trim());
+        }
+    }
+}
