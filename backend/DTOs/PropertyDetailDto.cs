32000fc4267debb08ae7d4a8537b1e4182f65589
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-09-30 16:07:41 +0530

    create propertyDetail Dto

---
+++ backend/DTOs/PropertyDetailDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using web_api.Models;
+
+namespace web_api.DTOs
+{
+    internal class PropertyDetailDto : PropertyListDto
+    {
+        public int CarpetArea { get; set; }
+        public string Address { get; set; }
+        public string Address2 { get; set; }
+        public int FloorNo { get; set; }
+        public int TotalFloors { get; set; }
+        public string MainEntrance { get; set; }
+        public int Security { get; set; }
+        public int Gated { get; set; }
+        public int Maintenance { get; set; }
+        public DateTime EstPossessionOn { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+    }
+}
