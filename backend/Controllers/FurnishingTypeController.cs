0b1b5c54110ce9de8063f8781f5cebeaacb50593
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-10-02 18:09:36 +0530

    create funishingType Controller

---
+++ backend/Controllers/FurnishingTypeController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web_api.DTOs;
+using web_api.Interfaces;
+using web_api.Models;
+
+namespace web_api.Controllers
+{
+    public class FurnishingTypeController : BaseController
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public FurnishingTypeController(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+         
+        public async Task<IActionResult> GetFunishingTypes()
+        {
+            var furnishingTypes = await unitOfWork.furnishingTypeRepository.GetFurnishingTypesAsync();
+            var furnishingTypesDto = mapper.Map<IEnumerable<KeyValuePairDto>>(furnishingTypes);
+            return Ok(furnishingTypesDto);
+        }
+    }
+}
