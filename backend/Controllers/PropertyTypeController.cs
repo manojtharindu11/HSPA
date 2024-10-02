d7a9ca001a19b13dc628dd1629fa43090d7875a0
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-10-02 17:59:12 +0530

    create property type controller

---
+++ backend/Controllers/PropertyTypeController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web_api.DTOs;
+using web_api.Interfaces;
+
+namespace web_api.Controllers
+{
+    public class PropertyTypeController : BaseController
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public PropertyTypeController(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        [HttpGet("list")]
+        [AllowAnonymous]
+
+        public async Task<IActionResult> GetPropertyTypes()
+        {
+            var propertyTypes = await unitOfWork.propertyTypeRepository.GetPropertyTypeAsync();
+            var propertyTypesDto = mapper.Map<IEnumerable<KeyValuePairDto>>(propertyTypes);
+            return Ok(propertyTypesDto);
+        }
+    }
+}
